14f63801c9f3aa18f19992922f0015d055082c67
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0200

    hawaiipl: Make Android.mk search recursive
    Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- Android.mk
+++ Android.mk
@@ -7,7 +7,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter hawaiipl, $(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 VENDOR_SYMLINKS := \
     $(TARGET_OUT_VENDOR)/lib/hw \
