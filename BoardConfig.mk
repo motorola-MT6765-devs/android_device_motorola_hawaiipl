3c0720e1ac7f71520cfb4f577da4f472a1246758
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-12-07 17:21:27 +0100

    hawaiipl: Reboot to recovery if init fails
    Change-Id: I1d8f38620caa7b003911ff30f69ed7973c100e6d
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,7 @@ TARGET_SCREEN_DENSITY := 280
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_PAGESIZE := 2048
