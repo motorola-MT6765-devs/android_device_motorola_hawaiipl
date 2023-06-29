423fae2d413be06bc92a57daa9bca333a592378d
Sushrut1101 <guptasushrut@gmail.com>
2023-06-30 03:40:05 +0530

    hawaiipl: Build some Bluetooth libraries from source
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -88,6 +88,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
+    audio.bluetooth.default \
+    libbluetooth_audio_session
+
 # Boot Control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-service
