e58b3c03cec307e2a2bd017921e7d0080060ec64
Sushrut1101 <guptasushrut@gmail.com>
2023-07-02 15:35:16 +0530

    hawaiipl: Build some WiFi blobs from source
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -274,6 +274,13 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor:64
 
 # WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service-lazy \
+    android.hardware.tetheroffload.config@1.0.vendor:64 \
+    android.hardware.tetheroffload.control@1.1.vendor:64 \
+    android.hardware.wifi.hostapd@1.3.vendor:64 \
+    android.hardware.wifi.supplicant@1.4.vendor:64
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
