e2aa7477011d87727099b5f561e9316eb38528b7
Sushrut1101 <guptasushrut@gmail.com>
2023-07-02 16:13:22 +0530

    hawaiipl: Build `libcamera2ndk_vendor`
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.6.vendor:64 \
-    android.hardware.camera.provider@2.6.vendor:64
+    android.hardware.camera.provider@2.6.vendor:64 \
+    libcamera2ndk_vendor
 
 # DRM
 PRODUCT_PACKAGES += \
